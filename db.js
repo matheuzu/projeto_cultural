60052e41c8eb355a2fbcde41125f302a1d0f2485
matheuzu <maestronalta@gmail.com>
2024-06-29 18:33:39 -0400

    modal

---
+++ db.js
@@ -0,0 +1,6 @@
+const filmes = [{
+    titulo: "Eles não podem gritaraaaaaaa", 
+    sinopse: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem excepturi est molestias iure natus sunt voluptate, alias similique hic, perspiciatis, amet in fugiat quae impedit. Blanditiis dolorum eum officia dicta?",
+    imgURL: "imagens/capa_demo.png",
+    link:"https://www.youtube.com/watch?v=-ncjkrIfA0g&t=702s"
+}]
--- script.js
+++ script.js
@@ -6,3 +6,27 @@ window.addEventListener('scroll', ()=> {
         nav.classList.remove("nav_black");
     }
 })
+
+function abrirModal(id) {
+    const modal = document.getElementById('modal');
+    const modalContent = document.getElementById('modal-content');
+
+    modalContent.innerHTML = `
+        <h2>${filmes[id].titulo}</h2>
+        <button onclick="fecharModal()" class="exit">X</button>
+        <img src="${filmes[id].imgURL}">
+        <p>${filmes[id].sinopse}</p>
+        <a href="${filmes[id].link}" class="watch" target="_blank"> Assistir </a> 
+        `;
+
+    modal.style.display = 'block';
+}   
+
+function fecharModal() {
+    const modal = document.getElementById('modal');
+    const modalContent = document.getElementById('modal-content');
+
+    modalContent.innerHTML = '';
+
+    modal.style.display = 'none';
+}
