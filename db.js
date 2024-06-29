0ad8dfea845b878b464b715ec9975023afff1975
matheuzu <maestronalta@gmail.com>
2024-06-29 19:01:13 -0400

    modal

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const filmes = [{
-    titulo: "Eles não podem gritaraaaaaaa", 
+    titulo: "Eles não podem gritar", 
     sinopse: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem excepturi est molestias iure natus sunt voluptate, alias similique hic, perspiciatis, amet in fugiat quae impedit. Blanditiis dolorum eum officia dicta?",
-    imgURL: "imagens/capa_demo.png",
+    imgURL: "imagens/eles_nao_podem_gritar.jpeg",
     link:"https://www.youtube.com/watch?v=-ncjkrIfA0g&t=702s"
 }]
