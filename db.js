d1110ff4a3e36d246daced126142794eb8976393
matheuzu <maestronalta@gmail.com>
2024-07-09 18:24:36 -0400

    quase la

--- db.js
+++ db.js
@@ -1,31 +1,31 @@
 const filmes = [{
     titulo: "Eles não podem gritar", 
     sinopse: "Uma equipe de jovens pesquisadores se aventura por pelo interior de Tefé (AM) para concluir um trabalho de universidade sobre o povo ribeirinho, porém ao entrar na mata e dar de cara com os perigos da floresta eles se deparam com desafios que não imaginavam",
-    imgURL: "imagens/eles_nao_podem_gritar.jpeg",
+    imgURL: "imagens/eles_nao_podem_gritar.png",
     link:"https://www.youtube.com/watch?v=-ncjkrIfA0g&t=702s"
 },
 {
     titulo: "Ribeirinhos do asfalto", 
     sinopse: "Deisy mora na ilha do Combu, em frente a Belém, do outro lado do rio. Ela sonha em morar na cidade, em meio às luzes que vê de sua casa na entrada da mata. Com a ajuda da mãe, tenta realizar seu desejo.",
-    imgURL: "imagens/eles_nao_podem_gritar.jpeg",
-    link:"https://www.youtube.com/watch?v=uSahFJE0Q2E"
+    imgURL: "imagens/ribeirinhos_do_asfalto.png",
+    link:"https://www.youtube.com/watch?v=EUQe5bojOQ8"
 },
 {
     titulo: "Jamakaru", 
     sinopse: "Uma doença misteriosa acomete os moradores de uma comunidade ao interior de Pernambuco, sem saber os motivos da infecção misteriosa, uma menina e sua avó buscam por cura, indo atrás de plantas e ervas indicadas por uma velha senhora, que afirma que o mal vai além do naturale está com os espiritos",
-    imgURL: "imagens/jamakaru.jpeg",
+    imgURL: "imagens/jamakaru.png",
     link:"https://vimeo.com/417445939"
 },
 {
     titulo: "A menina da ilha", 
     sinopse: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem excepturi est molestias iure natus sunt voluptate, alias similique hic, perspiciatis, amet in fugiat quae impedit. Blanditiis dolorum eum officia dicta?",
-    imgURL: "imagens/a_menina_da_ilha.jpeg",
+    imgURL: "imagens/a_menina_da_ilha.png",
     link:"https://www.youtube.com/watch?v=78sIZhEVpKw"
 },
 {
     titulo: "O menino que tinha medo do rio", 
-    sinopse: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem excepturi est molestias iure natus sunt voluptate, alias similique hic, perspiciatis, amet in fugiat quae impedit. Blanditiis dolorum eum officia dicta?",
-    imgURL: "imagens/eles_nao_podem_gritar.jpeg",
+    sinopse: "O curta-metragem mostra as belezas do Rio São Francisco, a lenda da mãe 'água, a ilha de São Félix e seus atrativos turísticos, e ainda os dramas vívidos por uma menina, que sonha em ser cantora.",
+    imgURL: "imagens/o_menino_que_tinha_medo_do_rio.png",
     link:"https://vimeo.com/439277177"
 }
 ]
